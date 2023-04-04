67810f8734dcd876140c3ce8b3df28b11b092621
Nurezkyagam27 <127917846+Nurezkyagam27@users.noreply.github.com>
2023-04-04 18:44:05 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.TaxiInfo = "true";
+            taxi.NumPassenger = 10;
+
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public string OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", OnDuty);
+            Console.WriteLine("Number Passenger: {0}", NumPassenger);
+            
+        }
+
+        public void PickUpPassenger()
+        {
+
+            Console.WriteLine("{0} sedang mengantar penumpang\n", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} sedang mengantar penumpang", DriverName);
+        }
+    }
+}
